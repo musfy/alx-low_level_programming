fa4e36bf951244afedb24059ee3081f9fddf34c4
Mustafa <misahkbai@gmail.com>
2022-03-10 20:01:09 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ *main - Entry point
+ *My hello, printf program
+ *Return: return 1 after executing
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
