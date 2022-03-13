25a3bba7b5439959900105ae9b69da0c45450110
Mustafa <misahkbai@gmail.com>
2022-03-12 18:59:14 -0800

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ **/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
