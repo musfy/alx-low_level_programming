bbb88c4b6f65f384ae836a36223bd8beef6af48e
Mustafa <misahkbai@gmail.com>
2022-03-17 09:30:02 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,11 @@
+/**
+ * function _isalpha(int c) - Returns 1 for alphabet and 0 otherwise
+ *
+ **/
 int _isalpha(int c)
 {
-	if ( c >= 'A' && c <= 'z')
-		if ( c <= 'Z' || c >= 'a') 
+	if (c >= 'A' && c <= 'z')
+		if (c <= 'Z' || c >= 'a') 
 			return (1);
 		else
 			return (0);
