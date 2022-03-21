e4a3d6ec358eb18ee3e9fe359fb6901b17a50eeb
Mustafa <misahkbai@gmail.com>
2022-03-21 10:25:06 -0700

    a

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * positive_or_negative: function to print postive or negative number
+ * @n: number passed as parameter
+ **/
+void positive_or_negative(int n)
+{	
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	else
+	printf("%d is zero\n", n);
+}
