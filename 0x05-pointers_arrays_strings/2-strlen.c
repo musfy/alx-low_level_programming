7c30aafcea6aa18e6225217721f75a88dd1be93b
Mustafa <misahkbai@gmail.com>
2022-04-02 09:27:25 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
+
