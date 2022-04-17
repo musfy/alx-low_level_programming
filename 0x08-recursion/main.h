4b2e90764e79d362a7951307377f179dc2035b13
Mustafa <misahkbai@gmail.com>
2022-04-17 03:17:45 -0700

    a

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
 int square(int n, int val);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif /* MAIN_H */
